6ba8dc9ee4881c9c7185db17d1024ed6762f4623
Pabloherresp <pabloherreraespinola@gmail.com>
2025-05-02 18:57:06 +0000

    First solution

--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 # from models import Person
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
@@ -16,27 +15,50 @@ CORS(app)
 # Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 
 # Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def handle_get_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body = {"family": members}
     return jsonify(response_body), 200
 
+@app.route('/members/<int:id>',methods=['GET'])
+def handle_get_specific_member(id):
+    member = jackson_family.get_member(id)
+    if(member):
+        response_body = member
+        return jsonify(response_body), 200
+    else:
+        response_body = {"error":"Family member not found."}
+        return jsonify(response_body), 404
+
+@app.route('/members', methods=['POST'])
+def handle_post_member():
+    new_member = request.json
+    added = jackson_family.add_member(new_member)
+    if added:
+        return handle_get_members()
+    else:
+        response_body = {"error":"There was an error adding the member."}
+        return jsonify(response_body), 400
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def handle_delete_member(id):
+    deleted = jackson_family.delete_member(id)
+    if deleted:
+        return handle_get_members()
+    else:
+        response_body = {"error":"Couldn't find a family member with that ID."}
+        return jsonify(response_body), 400
 
 
 # This only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -16,6 +16,18 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },{
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10,14,3]
+            },{
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -26,19 +38,32 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        initial_size = len(self._members)
+        self._members.append({
+            "id": self._generate_id(),
+            "first_name": member["first_name"],
+            "last_name" : self.last_name,
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"]
+        })
+        if len(self._members) == (initial_size+1):
+            return True
+        
+        return False
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for item in range(len(self._members)):
+            if self._members[item]["id"] == id:
+                self._members.pop(item)
+                return True
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for item in self._members:
+            if item["id"] == id:
+                print("----- FOUND -------")
+                return item
+        return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
